f4c433be270f4a5375d8e84183a468889fd4bd0e
Daniel Morais <damorais@gmail.com>
2020-03-04 11:00:52 -0300

    Commit inicial - Exercício array

---
+++ Exercicio1-LP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio1_LP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = {
+                "Dino da Silva Sauro",
+                "Fran da Silva Sauro",
+                "Charlene da Silva Sauro",
+                "Bob da Silva Sauro",
+                "Baby da Silva Sauro"
+            };
+
+            foreach (var nome in nomes)
+                Console.WriteLine(nome);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
